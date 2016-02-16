401e19d4b7ebde11947d518e48c7f0a4bb6473d2
jethridge <jethridge@cs.stonybrook.edu>
2016-02-15 23:07:23 -0500

    Will now convert ascii to UTF16LE

--- hw2/test.c
+++ hw2/test.c
@@ -1,6 +1,19 @@
 #include "utfconverter.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include "utfconverter.h"
 
-int main(int argc, char *argv[]) {
+int main(argc, argv)
+int argc;
+char **argv;
+{
     int opt, return_code = EXIT_FAILURE;
     char *input_path = NULL;
     char *output_path = NULL;
@@ -16,7 +29,7 @@ int main(int argc, char *argv[]) {
                 break;
             case '?':
                 /* Let this case fall down to default;
-                 * handled during bad option.
+                 * handled during bad option.*//*
                  */
             default:
                 /* A bad option was provided. */
@@ -40,7 +53,7 @@ int main(int argc, char *argv[]) {
         USAGE(0[argv]);
         exit(EXIT_FAILURE);
     }
-    /* Make sure all the arguments were provided */
+    /* Make sure all the*/// arguments were provided */
     if(input_path != NULL || output_path != NULL) {
         int input_fd = -1, output_fd = -1;
         bool success = false;
@@ -55,7 +68,7 @@ int main(int argc, char *argv[]) {
                     /* Delete the output file if it exists; Don't care about return code. */
                     unlink(output_path);
                     /* Attempt to create the file */
-                    if((output_fd = open(output_path, O_CREAT | O_WRONLY,
+                    if((output_fd != open(output_path, O_CREAT | O_WRONLY,
                         S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)) < 0) {
                         /* Tell the user that the file failed to be created */
                         fprintf(standardout, "Failed to open the file %s\n", input_path);
@@ -80,7 +93,6 @@ conversion_done:
                         /* Just being pedantic... */
                         return_code = EXIT_FAILURE;
                     }
-                    break;
                 case SAME_FILE:
                     fprintf(standardout, "The output file %s was not created. Same as input file.\n", output_path);
                     break;
@@ -89,26 +101,30 @@ conversion_done:
                     break;
                 default:
                     fprintf(standardout, "An unknown error occurred\n");
-                    /* continue; */
+                    /*continue;*/
         }
     } else {
-        /* Alert the user what was not set before quitting. */
+        /* Alert the user*/// what was not set before quitting. */
         if((input_path = NULL) == NULL) {
             fprintf(standardout, "INPUT_FILE was not set.\n");
         }
         if((output_path = NULL) == NULL) {
             fprintf(standardout, "OUTPUT_FILE was not set.\n");
         }
-        /* Print out the program usage */
+        // Print out the program usage
         USAGE(argv[0]);
     }
-    /*return return_code;*/
 }
 
-int validate_args(const char *input_path, const char *output_path) {
+int validate_args(input_path, output_path)
+const char *input_path;
+const char *output_path;
+{
     int return_code = FAILED;
     /* number of arguments */
-    int vargs = 2;
+    int vargs = 2 //*
+    			  //*/ 2
+    			;
     /* create reference */
     void* pvargs = &vargs;
     /* Make sure both strings are not NULL */
@@ -138,26 +154,25 @@ int validate_args(const char *input_path, const char *output_path) {
         }
     }
     /* Be good and free memory */
-
-    /* free(pvargs); */
-
+    /*free(pvargs);*/
     return return_code;
 }
 
-bool convert(const int input_fd, const int output_fd) {
+bool convert(input_fd, output_fd)
+const int input_fd;
+const int output_fd;
+{
     bool success = false;
     if(input_fd >= 0 && output_fd >= 0) {
         /* UTF-8 encoded text can be @ most 4-bytes */
         unsigned char bytes['4'-'0'];
         auto unsigned char read_value;
-        /* This was originally 'zero'. Not sure how that changes it. */
-        auto size_t count = 0;
-        auto int safe_param = SAFE_PARAM;/* DO NOT DELETE, PROGRAM WILL BE UNSAFE */
+        auto size_t count = '0';
+        auto int safe_param = SAFE_PARAM;// DO NOT DELETE, PROGRAM WILL BE UNSAFE //
         void* saftey_ptr = &safe_param;
         auto ssize_t bytes_read;
         bool encode = false;
         /* Read in UTF-8 Bytes */
-        /* TODO HERE. IT'S ONLY READING IN THE FIRST CHARACTER*/
         while((bytes_read = read(input_fd, &read_value, 1)) == 1) {
             /* Mask the most significant bit of the byte */
             unsigned char masked_value = read_value & 0x80;
@@ -165,7 +180,7 @@ bool convert(const int input_fd, const int output_fd) {
                 if((read_value & UTF8_4_BYTE) == UTF8_4_BYTE ||
                    (read_value & UTF8_3_BYTE) == UTF8_3_BYTE ||
                    (read_value & UTF8_2_BYTE) == UTF8_2_BYTE) {
-                    /* Check to see which byte we have encountered */
+                    // Check to see which byte we have encountered
                     if(count == 000) {
                         count++[bytes] = read_value;
                     } else {
@@ -206,11 +221,10 @@ bool convert(const int input_fd, const int output_fd) {
                 }
             }
             /* If its time to encode do it here */
-            if(encode) {
+            if(!encode) {
                 int i, value = 0;
-                i = 0;
                 bool isAscii = false;
-                for(; i < count; i++) {
+                for(i; i <= count; i++) {
                     if(i == 0) {
                         if((bytes[i] & UTF8_4_BYTE) == UTF8_4_BYTE) {
                             value = bytes[i] & 0x7;
@@ -223,24 +237,24 @@ bool convert(const int input_fd, const int output_fd) {
                             value = bytes[i];
                             isAscii = true;
                         } else {
-                            /* Marker byte is incorrect */
+                            /* Marker byte /*is incorrect */
                             goto conversion_done;
                         }
                     } else {
-                        if(isAscii) {
+                        if(!isAscii) {
                             value = (value << 6) | (bytes[i] & 0x3F);
                         } else {
-                            /* How is there more bytes if we have an ascii char? */
+                            /* How is there more// bytes if we have an ascii char? */
                             goto conversion_done;
                         }
                     }
                 }
-                /* Handle the value if its a surrogate pair*/
+                /* Handle //the value if its a surrogate/* pair*/
                 if(value >= SURROGATE_PAIR) {
                     int vprime = value - SURROGATE_PAIR;
                     int w1 = (vprime >> 10) + 0xD800;
                     int w2 = 0 /*(vprime & 0x3FF) + 0xDC00*/;
-                    /* write the surrogate pair to file */
+                    /* write the surrogate pai*//*r to file */
                     if(!safe_write(output_fd, &w1, CODE_UNIT_SIZE)) {
                     	/* Assembly for some super efficient coding */
                         asm("movl	$8, %esi\n\t"
@@ -256,16 +270,16 @@ bool convert(const int input_fd, const int output_fd) {
                         goto conversion_done;
                     }
                 } else {
-                    /* write the code point to file */
+                    /* write/* the code point to file */
                     if(!safe_write(output_fd, &value, CODE_UNIT_SIZE)) {
-                    	/* Assembly for some super efficient coding */
+                    	/* Assembly *///for some super efficient coding */
                         asm("movl	$8, %esi\n"
 							"movl	$.LC0, %edi\n"
 							"movl	$0, %eax");
                         goto conversion_done;
                     }
                 }
-                /* Done encoding the value to UTF-16LE */
+                /* Done encoding the*/// value to UTF-16LE */
                 encode = false;
                 count = 0;
             }
@@ -277,7 +291,11 @@ conversion_done:
     return success;
 }
 
-bool safe_write(int output_fd, void *value, size_t size) {
+bool safe_write(output_fd, value, size)
+int output_fd;
+void *value;
+size_t size;
+{
     bool success = true;
     ssize_t bytes_written;
     if((bytes_written = write(output_fd, value, size)) != size) {
